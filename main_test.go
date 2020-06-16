07620a0dc2d3f8e43bf9c8262b9dc496012fa748
vymiheev <vymiheev@gmail.com>
2020-06-16 23:56:13 +0300

    homework

--- main_test.go
+++ main_test.go
@@ -5,40 +5,40 @@ import (
 	"testing"
 )
 
-const testFullResult = `├───project
-│	├───file.txt (19b)
-│	└───gopher.png (70372b)
-├───static
-│	├───a_lorem
-│	│	├───dolor.txt (empty)
-│	│	├───gopher.png (70372b)
-│	│	└───ipsum
-│	│		└───gopher.png (70372b)
-│	├───css
-│	│	└───body.css (28b)
-│	├───empty.txt (empty)
-│	├───html
-│	│	└───index.html (57b)
-│	├───js
-│	│	└───site.js (10b)
-│	└───z_lorem
-│		├───dolor.txt (empty)
-│		├───gopher.png (70372b)
-│		└───ipsum
-│			└───gopher.png (70372b)
-├───zline
-│	├───empty.txt (empty)
-│	└───lorem
-│		├───dolor.txt (empty)
-│		├───gopher.png (70372b)
-│		└───ipsum
-│			└───gopher.png (70372b)
-└───zzfile.txt (empty)
+const testFullResult = `├─── project
+│  ├─── file.txt (19b)
+│  └─── gopher.png (70372b)
+├─── static
+│  ├─── a_lorem
+│  │  ├─── dolor.txt (empty)
+│  │  ├─── gopher.png (70372b)
+│  │  └─── ipsum
+│  │    └─── gopher.png (70372b)
+│  ├─── css
+│  │  └─── body.css (28b)
+│  ├─── empty.txt (empty)
+│  ├─── html
+│  │  └─── index.html (57b)
+│  ├─── js
+│  │  └─── site.js (10b)
+│  └─── z_lorem
+│    ├─── dolor.txt (empty)
+│    ├─── gopher.png (70372b)
+│    └─── ipsum
+│      └─── gopher.png (70372b)
+├─── zline
+│  ├─── empty.txt (empty)
+│  └─── lorem
+│    ├─── dolor.txt (empty)
+│    ├─── gopher.png (70372b)
+│    └─── ipsum
+│      └─── gopher.png (70372b)
+└─── zzfile.txt (empty)
 `
 
 func TestTreeFull(t *testing.T) {
 	out := new(bytes.Buffer)
-	err := dirTree(out, "testdata", true)
+	err := dirTree(out, "testdata", true, -1)
 	if err != nil {
 		t.Errorf("test for OK Failed - error")
 	}
@@ -48,23 +48,23 @@ func TestTreeFull(t *testing.T) {
 	}
 }
 
-const testDirResult = `├───project
-├───static
-│	├───a_lorem
-│	│	└───ipsum
-│	├───css
-│	├───html
-│	├───js
-│	└───z_lorem
-│		└───ipsum
-└───zline
-	└───lorem
-		└───ipsum
+const testDirResult = `├─── project
+├─── static
+│  ├─── a_lorem
+│  │  └─── ipsum
+│  ├─── css
+│  ├─── html
+│  ├─── js
+│  └─── z_lorem
+│    └─── ipsum
+└─── zline
+  └─── lorem
+    └─── ipsum
 `
 
 func TestTreeDir(t *testing.T) {
 	out := new(bytes.Buffer)
-	err := dirTree(out, "testdata", false)
+	err := dirTree(out, "testdata", false, -1)
 	if err != nil {
 		t.Errorf("test for OK Failed - error")
 	}
